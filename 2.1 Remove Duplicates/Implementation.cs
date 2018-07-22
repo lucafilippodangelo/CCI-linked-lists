aec0e40943541e0aebdc0bf5fc0c6602a18b7d26
Luca D'Angelo <infolucadangelo.it>
2018-07-22 16:47:05 +0100

    readme

---
+++ 2.1 Remove Duplicates/Implementation.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+
+namespace _2._1_Remove_Duplicates
+{
+    class Implementation
+    {
+        #region common methods
+
+        private int _tapB = 0;
+        private int _tapC = 0;
+
+        private void Tap(int i)
+        {
+            if (i == 0)
+            {
+                _tapB++;
+            }
+            else
+            {
+                _tapC++;
+            }
+        }
+
+        #endregion
+
+        private void DeleteDuplicatesUsingDictionary(LinkedListNode node)
+        {
+            var table = new Dictionary<int, bool>();
+            LinkedListNode previous = null;
+
+            while (node != null)
+            {
+                if (table.ContainsKey(node.Data))
+                {
+                    if (previous != null)
+                    {
+                        previous.Next = node.Next;
+                    }
+                }
+                else
+                {
+                    table.Add(node.Data, true);
+                    previous = node;
+                }
+
+                node = node.Next;
+            }
+        }
+
+    }//LD close Implementation class
+}//LD close namespace
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# CCI-linked-lists
+# Linked Lists
+
+## 2.1 Remove Duplicates: 
+description
+
+SOLUTION:
+-
+
+USEFUL LINKS:
+- Linked list class and node
+  - https://msdn.microsoft.com/en-us/library/he2s3bh7(v=vs.110).aspx
+  - https://msdn.microsoft.com/en-us/library/ahf4c754(v=vs.110).aspx
