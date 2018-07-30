adf53e6156b283b3a97712b4347ab433fd0e3f6f
Luca D'Angelo <infolucadangelo.it>
2018-07-30 07:37:48 +0100

    2.5

---
+++ 2.5 Sum Lists/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _2._5_Sum_Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
