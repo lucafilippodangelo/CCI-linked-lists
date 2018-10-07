657f8bd036671bbc0b2585869365aeda06d1f73e
Luca D'Angelo <infolucadangelo.it>
2018-10-07 12:54:04 +0100

    2.5

---
+++ 2.5 Sum Lists/Implementation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _2._5_Sum_Lists
+{
+    class Implementation
+    {
+        public static LinkedList<int> Partition(LinkedListNode<int> aLinkedListNode, int n)
+        {
+            //LD create the support linked list
+            LinkedList<int> supportLinkedList = new LinkedList<int>();
+
+            while (aLinkedListNode != null)
+            {
+                if (aLinkedListNode.Value < n)
+                {
+                    supportLinkedList.AddFirst(aLinkedListNode.Value);
+                }
+                else
+                {
+                    supportLinkedList.AddLast(aLinkedListNode.Value);
+                }
+                aLinkedListNode = aLinkedListNode.Next;
+            }
+            return supportLinkedList;
+        }
+    }
+}
