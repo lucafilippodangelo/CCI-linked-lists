4b02053bababb64922f2a6594af611664f7111b6
Luca D'Angelo <infolucadangelo.it>
2018-10-07 16:40:37 +0100

    2.5 done

--- 2.5 Sum Lists/Implementation.cs
+++ 2.5 Sum Lists/Implementation.cs
@@ -6,24 +6,32 @@ namespace _2._5_Sum_Lists
 {
     class Implementation
     {
-        public static LinkedList<int> Partition(LinkedListNode<int> aLinkedListNode, int n)
+        public static LinkedList<int> sumTwoLinkedList(LinkedListNode<int> l1Node, LinkedListNode<int> l2Node, LinkedList<int> supportList, int carry)
         {
-            //LD create the support linked list
-            LinkedList<int> supportLinkedList = new LinkedList<int>();
+        //LD if l1 or l2 are empty then return
+        if (l1Node == null && l2Node == null && carry==0)
+            {return null; }
 
-            while (aLinkedListNode != null)
+            //LD sum all needed for the current value
+            int currentValue = carry;
+            currentValue += l1Node.Value;
+            currentValue += l2Node.Value;
+
+            //LD create a node for the current value
+            LinkedListNode<int> result = new LinkedListNode<int>(currentValue % 10); //second digit of the number. We will carry the 
+
+            //LD add the node to the support list
+            supportList.AddLast(result);
+
+            //LD RECURSIVE CALL for next nodes and carry
+            if (l1Node.Next != null || l1Node.Next != null)
             {
-                if (aLinkedListNode.Value < n)
-                {
-                    supportLinkedList.AddFirst(aLinkedListNode.Value);
-                }
-                else
-                {
-                    supportLinkedList.AddLast(aLinkedListNode.Value);
-                }
-                aLinkedListNode = aLinkedListNode.Next;
+                sumTwoLinkedList(l1Node.Next, l2Node.Next, supportList, currentValue >= 10 ? 1 : 0);
             }
-            return supportLinkedList;
+
+            //LD return whe no more nodes
+            return supportList;
+
         }
     }
 }
--- 2.5 Sum Lists/Program.cs
+++ 2.5 Sum Lists/Program.cs
@@ -6,13 +6,22 @@ namespace _2._5_Sum_Lists
     {
         static void Main(string[] args)
         {
-            //LD Linked list creation
-            int[] numbers = { 3, 7, 4, 8, 1, 9 };
-            var LinkedList = Common.Utilities.createLinkedListFromArrayInt(numbers);
-            Common.Utilities.displayFullLinkedListInt(LinkedList, "Linked List: ");
+            //LD Linked list One, Two and Three creation. List Three is Empty
+            int[] n1 = { 7,1,6};
+            var l1 = Common.Utilities.createLinkedListFromArrayInt(n1);
+            Common.Utilities.displayFullLinkedListInt(l1, "Linked List One: ");
 
-            var LinkedListProcessed = Implementation.Partition(LinkedList.First, 8);
-            Common.Utilities.displayFullLinkedListInt(LinkedListProcessed, "Linked List Processed, expected 1,4,7,3,8,9: ");
+            int[] n2 = { 5,9,2 };
+            var l2 = Common.Utilities.createLinkedListFromArrayInt(n2);
+            Common.Utilities.displayFullLinkedListInt(l2, "Linked List Two: ");
+
+            int[] n3 = { };
+            var sl = Common.Utilities.createLinkedListFromArrayInt(n3); //sl -> "Support List"
+            Common.Utilities.displayFullLinkedListInt(sl, "Linked List Three when created: ");
+
+            //LD Call Function
+            var result = Implementation.sumTwoLinkedList(l1.First,l2.First,sl,0);
+            Common.Utilities.displayFullLinkedListInt(sl, "Result Linked List : ");
 
             Console.ReadLine();
         }
